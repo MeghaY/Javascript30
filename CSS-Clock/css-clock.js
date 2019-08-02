699ac74cfaeb6a2ac2077094b2369d43be1af4d0
MeghaY <desaimegha14@gmail.com>
2019-08-02 13:48:55 -0700

    Add css-clock implementation

--- CSS-Clock/css-clock.js
+++ CSS-Clock/css-clock.js
@@ -2,24 +2,28 @@
 
 const secondHand = document.querySelector('.second-hand');
 const minutesHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('hour-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-function setDate () {
+function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    secondHand.style.transition = secondsDegrees === 90 ? 'all 0s' : 'all 0.05s';
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + 90;
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
     minutesHand.style.transform = `rotate(${minsDegrees}deg)`;
-    minutesHand.style.transition = minsDegrees === 90 ? 'all 0s' : 'all 0.05s';
-
 
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360) + 90;
+    console.log(hours);
+    const hoursDegrees = ((hours / 12) * 360) + ((mins / 60) * 30) + 90;
+    console.log(hoursDegrees);
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    secondHand.style.transition = secondsDegrees === 90 ? 'all 0s' : 'all 0.05s';
+    minutesHand.style.transition = minsDegrees === 90 ? 'all 0s' : 'all 0.01s';
+
 }
 
 setInterval(setDate, 1000);
+setDate();
