a2866019c2b37e2286b368618299a8b209c8e7e2
MeghaY <desaimegha14@gmail.com>
2020-04-16 12:15:36 -0700

    Adding bunch of other pure javascript, HTML, CSS feature related functionalities useful for app development

---
+++ Array-Cardio-2/index.js
@@ -0,0 +1,33 @@
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// Array.prototype.every() // is everyone 19 or older?
+console.log(people.reduce((accu, curr) => {
+    curr['age'] = new Date().getFullYear() - curr.year;
+    accu.push(curr);
+    return accu;
+}, []));
+console.log('Some', people.some(item => item.age >= 19));
+console.log('Some', people.every(item => item.age >= 19));
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
